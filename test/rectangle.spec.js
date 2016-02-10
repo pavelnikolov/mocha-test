95159ac00e1e81b358418da8514ee2086b3bc152
Pavel Nikolov <me@pavelnikolov.net>
2016-02-10 16:14:53 +1100

    Added code coverage with isparta

--- test/rectangle.spec.js
+++ test/rectangle.spec.js
@@ -22,33 +22,112 @@ import Rectangle from '../rectangle';
 // anymore. In this case we are not using "this" so the new
 // syntax is just used for brevity.
 describe('Rectangle', () => {
+  describe('#constructor()', () => {
+    it('requires two numerical arguments', () => {
+      let failFn = () => {
+        new Rectangle();
+      };
+      failFn.should.throw(Error);
+
+      failFn = () => {
+        new Rectangle(1.0);
+      };
+      failFn.should.throw(Error);
+
+      failFn = () => {
+        new Rectangle('foo', 'bar');
+      };
+      failFn.should.throw(Error);
+
+      failFn = () => {
+        new Rectangle(5, 7);
+      };
+      failFn.should.not.throw(Error);
+    });
+  });
+
   describe('#width', () => {
     let rectangle;
 
     beforeEach(() => {
-      // Create a new Rectangle object before every test.
       rectangle = new Rectangle(10, 20);
     });
 
     it('returns the width', () => {
-      // This will fail if "rectangle.width" does
-      // not equal 10.
       rectangle.width.should.equal(10);
     });
 
     it('can be changed', () => {
-      // Assert that the width can be changed.
       rectangle.width = 30;
       rectangle.width.should.equal(30);
     });
 
     it('only accepts numerical values', () => {
-      // Assert that an error will be thrown if
-      // the width it set to a non-numerical value.
       let failFn = () => {
         rectangle.width = 'foo';
       };
+      failFn.should.throw(Error);
+    });
+  });
 
+  describe('#height', () => {
+    let rectangle;
+
+    beforeEach(() => {
+      rectangle = new Rectangle(10, 20);
+    });
+
+    it('returns the height', () => {
+      rectangle.height.should.equal(20);
+    });
+
+    it('can be changed', () => {
+      rectangle.height = 30;
+      rectangle.height.should.equal(30);
+    });
+
+    it('only accepts numerical values', () => {
+      let failFn = () => {
+        rectangle.height = 'foo';
+      };
+      failFn.should.throw(Error);
+    });
+  });
+
+  describe('#area', () => {
+    let rectangle;
+
+    beforeEach(() => {
+      rectangle = new Rectangle(10, 20);
+    });
+
+    it('returns the area', () => {
+      rectangle.area.should.equal(200);
+    });
+
+    it('can not be changed', () => {
+      let failFn = () => {
+        rectangle.area = 1000;
+      };
+      failFn.should.throw(Error);
+    });
+  });
+
+  describe('#circumference', () => {
+    let rectangle;
+
+    beforeEach(() => {
+      rectangle = new Rectangle(10, 20);
+    });
+
+    it('returns the circumference', () => {
+      rectangle.circumference.should.equal(60);
+    });
+
+    it('can not be changed', () => {
+      let failFn = () => {
+        rectangle.circumference = 1000;
+      };
       failFn.should.throw(Error);
     });
   });
